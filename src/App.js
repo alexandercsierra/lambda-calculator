0f2e03c74eae9c9c89ca2d16c5405b5436d35850
Alexander Sierra <alexandercsierra@gmail.com>
2019-12-03 20:28:38 -0500

    buttons set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Display from "./components/DisplayComponents/Display";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -13,11 +17,18 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
+
+  const [numberState, setNumberState] = useState("");
+  const [operatorState, setOperatorState] =  useState("");
+
   return (
     <div className="container">
       <Logo />
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display numberState = {numberState} operatorState = {operatorState}/>
+        <Numbers setNumberState = {setNumberState}/>
+        <Operators setOperatorState = {setOperatorState}/>
+        <Specials />
       </div>
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,19 +1,18 @@
 import React from "react";
-
-//import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
-//Import your array data to from the provided data file
+import {numbers} from "../../../data";
 
 const Numbers = () => {
-  // STEP 2 - add the imported data to state
+  const numBtn = numbers.map(e => {
+    return(<button>{e}</button>);
+  })
+
   return (
+    
+
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    {numBtn}
     </div>
   );
 };
+
+export default Numbers;
